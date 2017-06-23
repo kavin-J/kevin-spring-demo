6226d2c02a796f32f0e99a9bdde4a958b32e0c96
江海洋 <jianghaiyang@ccclubs.com>
2017-06-23 15:34:08 +0800

    kafka practice

---
+++ springboot-kafka-demo/src/main/java/com/kevin/demo/SpringbootKafkaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.kevin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaDemoApplication.class, args);
+	}
+}
