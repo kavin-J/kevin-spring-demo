939eabd33219380dcd9fe95848f785d4b2d5f3e6
Jkevin <2078724466@qq.com>
2017-07-07 01:34:32 +0800

    注解校验bean

---
+++ springboot-validate-demo/src/main/java/com/kevin/IssueOrderInput.java
@@ -0,0 +1,119 @@
+package com.kevin;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+/**
+ * @author jianghaiyang
+ * @create 2017-06-30
+ **/
+public class IssueOrderInput implements java.io.Serializable {
+
+    @NotNull(message = "vin码必填")
+    private String vin;
+
+    @NotNull(message = "订单必填")
+    private Long orderId;
+
+    @NotNull(message = "订单开始时间(yyyy-MM-dd HH:mm:ss)必填")
+    @Pattern(regexp = "^(\\d{4})-([0-1]\\d)-([0-3]\\d)\\s([0-5]\\d):([0-5]\\d):([0-5]\\d)$", message = "日期格式匹配不正确(yyyy-MM-dd HH:mm:ss)")
+    private String startTime;
+
+    @NotNull(message = "订单结束时间(yyyy-MM-dd HH:mm:ss)必填")
+    private String endTime;
+
+    @NotNull(message = "RFID号(8位)必填")
+    @Length(min = 8, max = 8, message = "RFID号(8位)必填")
+    private String rfid;
+
+    @NotNull(message = "授权码(6位)必填")
+    @Min(value = 100000, message = "授权码最小值 100000")
+    @Max(value = 999999, message = "授权码最大值 999999")
+    private Integer authCode;
+
+    //@Pattern(regexp = "/^(0|1)$/")
+    private Integer flag;
+
+    private String appId;
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getRfid() {
+        return rfid;
+    }
+
+    public void setRfid(String rfid) {
+        this.rfid = rfid;
+    }
+
+    public Integer getAuthCode() {
+        return authCode;
+    }
+
+    public void setAuthCode(Integer authCode) {
+        this.authCode = authCode;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public Integer getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Integer flag) {
+        this.flag = flag;
+    }
+
+    @Override
+    public String toString() {
+        return "IssueOrderInput{" +
+                "vin='" + vin + '\'' +
+                ", orderId=" + orderId +
+                ", startTime='" + startTime + '\'' +
+                ", endTime='" + endTime + '\'' +
+                ", rfid='" + rfid + '\'' +
+                ", authCode=" + authCode +
+                ", appId='" + appId + '\'' +
+                '}';
+    }
+}
---
+++ springboot-validate-demo/src/test/java/com/kevin/TestValidate.java
@@ -0,0 +1,66 @@
+package com.kevin;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by mypos on 2017/7/7.
+ */
+public class TestValidate {
+    private static Validator validator;
+
+    @BeforeClass
+    public static void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void manufacturerIsNull() {
+        IssueOrderInput input = new IssueOrderInput();
+        input.setAppId("123");
+        input.setAuthCode(100000);
+        input.setEndTime("2017-10-18 12:22:09");
+        input.setOrderId(1234567l);
+        input.setRfid("21235682");
+        input.setVin("2");
+        input.setStartTime("2009-10");
+        Set<ConstraintViolation<IssueOrderInput>> constraintViolations =
+                validator.validate(input);
+
+        assertEquals(1, constraintViolations.size());
+        assertEquals(
+                "格式匹配不正确(yyyy-MM-dd HH:mm:ss)",
+                constraintViolations.iterator().next().getMessage()
+        );
+    }
+
+    @Test
+    public void success() {
+
+        IssueOrderInput input = new IssueOrderInput();
+        input.setAppId("123");
+        input.setAuthCode(100000);
+        input.setEndTime("2017-10-18 12:22:09");
+        input.setOrderId(1234567l);
+        input.setRfid("21235682");
+        input.setVin("2");
+        input.setFlag(9);
+        input.setStartTime("2017-10-18 12:22:09");
+        Set<ConstraintViolation<IssueOrderInput>> constraintViolations =
+                validator.validate(input);
+
+        assertEquals(0, constraintViolations.size());
+
+
+    }
+}
